ca20c5f0f3431d23b34a72bdfcd48a72a962bbbc
victor <vhenrixon@gmail.com>
2020-05-06 12:59:00 -0400

    Before testing

--- src/node.py
+++ src/node.py
@@ -2,23 +2,27 @@ from bluetooth import *
 import random 
 import json 
 import ast
+import python_jwt as jwt, jwcrypto.jwk as jwk, datetime
+import uuid 
+
 
 class Node:
 
     def __init__(self, token, starting_prefix):
         # A node is a client and a server 
-        self.token = token 
+        self.token = token  #TODO: add verficiation of nodes
         self.socket = BluetoothSocket(RFCOMM)
         self.neighbors = [] # A list of neighbors that are nearby
         self.starting_prefix = if starting_prefix !=  3: "VVV"
         self.server_limits = 3
         self.server_port = 3
-        self.recv_limit = 1024
+        self.recv_limit = 2048
         self.server_address = None 
         self.connections = []
         self.current_topic = {}
         self.change_flag = False
         self.new_topic = {}
+        self.key = jwk.JWK.generate(kty='RSA', size=2048)
 
     def connect_to_neighbors(self):
         # All servers should have keyboard in there name
@@ -33,43 +37,47 @@ class Node:
 
     def setup_server(self):
         try:
-            self.socket.bind((self.start_prefix+self.create_number_series(), self.server_port))
+            self.server_address = self.start_prefix+"-"+uuid.getnode()
+            self.socket.bind((self.server_address, self.server_port))
             self.socket.listen(self.server_limits)        
         except BluetoothError:
             #TODO add a error 
             pass
 
-    def create_number_series(self):
-        rs = ""
-        for i in range(30):
-            rs = rs + str(i)
-        return rs
 
     def _client_tick(self):
         conn, addr = self.socket.accept()
         self.connections.append(conn)
-        # TODO: add verification via token     
         previous = None
         for node in connections: 
             data = node.recv(recv_limit)
-            cleaned_data = ast.literal_eval(data) 
+            header, claim = jwt.verify_jwt(data, self.key, ['PS256']) 
             if previous is None: 
-                previous = cleaned_data
-                self.current_topic = cleaned_data
+                previous = claim
             else: 
-                if previous['signature'] != cleaned_data['signature']:
-                    self.current_topic = cleaned_data
-            
+                if current_topic['uuid'] != claim['uuid']:
+                    self.currrent_topic = claim
+                    # TODO: write a system that determines which to choose either the claim or the current_topic
+                    
     def send_message(self, message):
         self.change_flag = True
+        message_base = {"content": {message}, "uuid": uuid.uuid1()}
+        self.new_topic = jwt.generate_jwt(message_base, self.key, 'PS256', datetime.timedelta(minutes=5)) 
         # TODO verfify that the message is written correctly to a standard or make a template 
-        # sigature system 
-
+        
     def tick(self): 
         self._client_tick()
         if self.change_flag:
-            self.socket.send(self.new_topic)
-            
+            self.socket.send(str(self.new_topic))
+            self.new_topic = {}
 
-
+    '''
+    Comeback because you would need to ask each node for this info 
+    def node_list(self):
+        print("| Node name | connection status | Server Port |")
+        print("-----------------------------------------------")
+        print("|"+self.server_address+"|"+"Current NODE |"+self.server_port+"|")
+        for nodes in self.connection:
+            print("|"+nodes.+)
+     '''   
 
